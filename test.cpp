6f610eb559dae1f719aece294fbd1708809147ac
ravindra_singh <rvsbana@gmail.com>
2016-12-24 00:00:00 +0530

    g++ test.cpp && ./.a.out

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+void t(int n){
+    if (n/2){
+        cout << n << endl;
+        t(n/2);
+    };
+    cout<<(n%2);
+}
+int main()
+{
+    t(10);
+    return 0;
+}
