342f6d44de07f3430282b57ac14c57fbc74aca6d
ravindra_singh <rvsbana@gmail.com>
2017-01-29 00:00:00 +0530

    gcc test.c && ./.a.out

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+int main()
+{
+    int a=54325;
+    char buffer[20];
+    itoa(a,buffer,2);   // here 2 means binary
+    printf("Binary value = %s\n", buffer);
+ 
+    itoa(a,buffer,10);   // here 10 means decimal
+    printf("Decimal value = %s\n", buffer);
+ 
+    itoa(a,buffer,16);   // here 16 means Hexadecimal
+    printf("Hexadecimal value = %s\n", buffer);
+    return 0;
+}
