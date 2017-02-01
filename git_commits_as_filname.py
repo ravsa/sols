0eeed5b04024ed5d4b0a65ab14c399d7384c324f
ravindra_singh <rvsbana@gmail.com>
2017-02-02 00:00:00 +0530

    python git_commits_as_filname.py

---
+++ git_commits_as_filname.py
@@ -0,0 +1,46 @@
+import os
+import datetime
+import random
+import glob
+files = list()
+for _file_name in glob.glob("*"):
+    files.append(_file_name)
+random.shuffle(files)
+files = zip(files[::2], files[1::2])
+for f1, f2 in files:
+    delta = datetime.timedelta(days=1)
+    date = str(datetime.datetime.now() + delta)
+    cur_date = '"' + ''.join(date.split()[0].split('-')) + '"'
+    setdate_cmd = 'sudo date +%Y%m%d -s ' + cur_date
+    print setdate_cmd
+    os.system(setdate_cmd)
+
+    git_add = 'git add ' + f1
+    if f1.endswith("py"):
+        git_commit = 'git commit -am "python ' + f1 + '"'
+    if f1.endswith("cpp"):
+        git_commit = 'git commit -am "g++ ' + f1 + ' && ./.a.out"'
+    if f1.endswith("c"):
+        git_commit = 'git commit -am "gcc ' + f1 + ' && ./.a.out"'
+    if f1.endswith("java"):
+        git_commit = 'git commit -am "javac ' + \
+            f1 + ' && java ' + f1.split('.')[0] + '"'
+    print git_add
+    os.system(git_add)
+    print git_commit
+    os.system(git_commit)
+
+    git_add = 'git add ' + f2
+    if f2.endswith("py"):
+        git_commit = 'git commit -am "python ' + f2 + '"'
+    if f2.endswith("cpp"):
+        git_commit = 'git commit -am "g++ ' + f2 + ' && ./.a.out"'
+    if f2.endswith("c"):
+        git_commit = 'git commit -am "gcc ' + f2 + ' && ./.a.out"'
+    if f2.endswith("java"):
+        git_commit = 'git commit -am "javac ' + \
+            f2 + ' && java ' + f2.split('.')[0] + '"'
+    print git_add
+    os.system(git_add)
+    print git_commit
+    os.system(git_commit)
